9c3106155d5d25f58bd5114f3e54e965f2bafb19
Cristian <c_valadez@outlook.com>
2022-04-11 15:58:08 -0500

    Try scraping bestbuy.com

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import bs4
 import requests
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
 
 
-def print_microcenter():
+def get_microcenter_data():
+    """
+        Get's data related to a searched item from microcenter.com in Westmont, IL
+    :return: status = [isRebate, isOnSale]
+    """
     # Hardcode link for now
     # url = 'https://www.microcenter.com/search/search_results.aspx?Ntk=all&sortby=match&rpp=96&N=4294966937+4294820651+4294808485&myStore=true&storeid=025'
     # page = requests.get(url)
@@ -11,14 +15,12 @@ def print_microcenter():
     # productGrid = soup.find(id="productGrid")
     # items = productGrid.find_all("div", class_="details")
     # list = results.find_all('li')
-
+    is_rebate = False
+    is_on_sale = False
     file = open("Test.html", 'r')
     contents = file.read()
     soup = BeautifulSoup(contents, "html.parser")
     items = soup.find_all("div", class_="details")
-    # file = open("Test.html", "w")
-    # file.write(page.text)
-    # file.close()
 
     print("Found {:d} at Microcenter".format(len(items)))
     for item in items:
@@ -31,6 +33,7 @@ def print_microcenter():
         # Deal with rebates
         buffer = price_wrapper.find("div", class_="rebate-price")
         if len(buffer.text) > 0:
+            is_rebate = True
             price_info += buffer.text + " : "
 
         # Print price (minus rebates)
@@ -39,13 +42,15 @@ def print_microcenter():
         # Deal with sales info
         buffer = price_wrapper.find("span", class_="strike")
         if buffer is not None:
+            is_on_sale = True
             price_info += " Was " + buffer.text
             buffer = price_wrapper.find("span", class_="savings")
             if buffer is not None:
                 price_info += " " + buffer.text
 
         # Extract the lowest price from price info, should be the first number in string
-        price = price_info[price_info.find('$'):price_info.find(' ')]
+        end_index = price_info.find(' ') if len(price_info) > 10 else len(price_info)
+        price = price_info[price_info.find('$') + 1:end_index]
 
         print("==============================================")
         print("Brand: {:s}\nID: {:s}\nName: {:s}\nPrice Info: {:s}\n"
@@ -54,10 +59,17 @@ def print_microcenter():
             price_info, stock, price))
 
 
-def print_bestbuy(url):
-    pass
+def print_bestbuy():
+    url = 'https://www.bestbuy.com/'
+    user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:83.0) Gecko/' \
+                                '20100101 Firefox/83.0'}
+    page = requests.get(url, user_agent)
+    print(page.status_code)
+    # file = open("BB_Extract.html", 'w')
+    # file.write(page.text)
+    # file.close()
 
 
 if __name__ == '__main__':
-    print_microcenter()
-    print_bestbuy('Test.html')
+    # get_microcenter_data()
+    print_bestbuy()
